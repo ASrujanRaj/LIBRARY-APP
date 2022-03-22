e5ed9007634211c8370a1dc47216345a32c6033b
ASrujanRaj <102138936+ASrujanRaj@users.noreply.github.com>
2022-03-22 13:02:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -113,40 +113,4 @@ module.exports = mongoose.model('getbook',getBook)
 
 
 
-//const { string } = require('joi');
-// mongoose.connect('mongodb://localhost/library')
-//     .then(() => console.log('connected to MongoDB.....'))
-//     .catch(err => console.error('could not connect to MongoDB....',err));
-
-
-// const getBook= new mongoose.Schema({
-//     bookName: String,
-//     bookAuthor: String,
-//     bookAvailable: Boolean
-// });    
-//const Course = mongoose.model('course',getBook);
-
-
-
-// async function createCourse(){
-
-// const course = new Course({
-//     bookName:"maths A",
-//     bookAuthor:"Ganesh",
-//     bookAvailable: true,
-
-// });
-
-
-// const result = await course.save();
-// console.log(result);
-// }
-// //createCourse();
-
-
-// async function getCourses(){
-//    const courses = await Course.find();
-//    console.log(courses);
-// }
-// getCourses();
 
